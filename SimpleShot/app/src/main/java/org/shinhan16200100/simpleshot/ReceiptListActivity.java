f82ec48f46c4c3ab40552420aabaf3047984e7c1
jihyunamykim <dkrnahs916@gmail.com>
2017-09-15 16:47:10 +0900

    메인 화면 완료

---
+++ SimpleShot/app/src/main/java/org/shinhan16200100/simpleshot/ReceiptListActivity.java
@@ -0,0 +1,13 @@
+package org.shinhan16200100.simpleshot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReceiptListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receipt_list);
+    }
+}
---
+++ SimpleShot/app/src/main/java/org/shinhan16200100/simpleshot/SpendingListActivity.java
@@ -0,0 +1,13 @@
+package org.shinhan16200100.simpleshot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SpendingListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spending_list);
+    }
+}
